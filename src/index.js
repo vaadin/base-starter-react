ee28967905c7e9441ba092c4091b6ff5ae840372
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2018-04-10 13:28:18 +0300

    Update polyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
+/** Polyfill required for web components **/
+import '@webcomponents/webcomponentsjs/webcomponents-sd-ce.js';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
